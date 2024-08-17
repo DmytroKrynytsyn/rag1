ff0c65c76d2a7ac2534f7a34d3d07dabc37b4916
DmytroKrynytsyn <dmitry.krinitsyn@gmail.com>
2024-08-17 12:34:03 +0200

    ports

--- rag1backend/src/rag1backend/entrypoint/main.py
+++ rag1backend/src/rag1backend/entrypoint/main.py
@@ -4,7 +4,7 @@ from ..service.rest_service import router
 
 def main():
     print("Service started")
-    uvicorn.run(router, host="0.0.0.0", port=8000)
+    uvicorn.run(router, host="0.0.0.0", port=80)
 
 if __name__ == "__main__":
     main()
